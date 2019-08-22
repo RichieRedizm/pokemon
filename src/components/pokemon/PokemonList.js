5473046447dcf37ace2b092d3b16ba9ac47e0590
Richie Redizm <richieredizm@gmail.com>
2019-08-22 17:04:37 +0100

    create react app and install Apollo for GraphQL

---
+++ src/components/pokemon/PokemonList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PokemonList = () => {
+  return (
+    <div className='card text-center'>
+      <h3>pokemon name</h3>
+    </div>
+  )
+}
+
+export default PokemonList
