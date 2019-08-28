a5158fa6d2b949a93d2616f6b34bfdf53029d631
Richie Redizm <richieredizm@gmail.com>
2019-08-28 13:44:26 +0100

    pokemon detail styling

--- src/components/pokemon/PokemonDetail.js
+++ src/components/pokemon/PokemonDetail.js
@@ -18,36 +18,72 @@ const PokemonDetail = ({ match }) => {
     return (
       <div className='detail-list'>
         {list.map(item => (
-          <span key={item}>{item}</span>
+          <span key={item}>{item} /</span>
         ))}
       </div>
     )
   }
+  const formatObjectList = object => {
+    return object.map((obj, index) => {
+      const { name, type, damage } = obj
+      return (
+        <div key={index} className='detail-object-list'>
+          <span>
+            <strong className='label-light'>Name:</strong> {name}
+          </span>
+          <span>
+            <strong className='label-light'>Type:</strong> {type}
+          </span>
+          <span>
+            <strong className='label-light'>Damage:</strong> {damage}
+          </span>
+        </div>
+      )
+    })
+  }
 
-  const { name, image, classification, types, resistant, weaknesses } = pokemon
+  const {
+    name,
+    image,
+    classification,
+    types,
+    resistant,
+    weaknesses,
+    attacks
+  } = pokemon
+
   return (
     <Fragment>
       <Link to={`/`} className='btn btn-light btn-sm my-1'>
         go back to search
       </Link>
-
-      {name && <h2>{name}</h2>}
-      {classification && <strong>{classification}</strong>}
-      <div className='detail-image'>
-        <img src={image} alt={name} />
+      <div className='detail-wrap'>
+        <div className='detail-meta'>
+          {name && <h2>{name}</h2>}
+          {classification && <h3 className='label-dark'>{classification}</h3>}
+        </div>
+        <div className='detail-image'>
+          <img src={image} alt={name} />
+        </div>
       </div>
       <div className='card grid-2'>
-        <div>
-          {types && <strong>Type:</strong>}
+        <div className='detail'>
+          {types && <strong className='label-dark'>Type:</strong>}
           {types && formatArrayList(types)}
-          {resistant && <strong>Resistant: </strong>}
+          {resistant && <strong className='label-dark'>Resistant: </strong>}
           {resistant && formatArrayList(resistant)}
-          {weaknesses && <strong>weaknesses: </strong>}
+          {weaknesses && <strong className='label-dark'>weaknesses: </strong>}
           {weaknesses && formatArrayList(weaknesses)}
         </div>
-        <div>
-          {weaknesses && <strong>weaknesses: </strong>}
-          {weaknesses && formatArrayList(weaknesses)}
+        <div className='detail'>
+          {attacks.fast && (
+            <strong className='label-dark'>Fast Attacks: </strong>
+          )}
+          {attacks.fast && formatObjectList(attacks.fast)}
+          {attacks.special && (
+            <strong className='label-dark'>Special Attacks: </strong>
+          )}
+          {attacks.special && formatObjectList(attacks.special)}
         </div>
       </div>
     </Fragment>
